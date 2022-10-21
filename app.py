751bdcdb0abab5f31ca2908b21a9b4885e48ac4d
Graham Misail <gmisail64@gmail.com>
2022-10-21 18:19:18 -0400

    Add *rough* current semester estimate

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request, render_template
+from datetime import date
 
 import db
 
 app = Flask(__name__)
 
-@app.route('/')
+def get_current_semester():
+    current_date = date.today()
+    start_month = "01"
+    
+    if current_date.month >= 5 and \
+        current_date.month < 8:
+        start_month = "05"
+    elif current_date.month >= 8:
+        start_month = "08"
+
+    return "%d%s" % (current_date.year, start_month)
+
+@app.route("/")
 def index():
     return "<p>hello world</p>"
 
-@app.route('/projects')
-def projects():
-    projects = db.get_semester_projects("202201", False)
+@app.route("/projects")
+@app.route("/projects/<semester>")
+def projects(semester=None):
+    semester = get_current_semester() if semester is None else semester
+     
+    projects = db.get_semester_projects(semester, False)
     return render_template("projects.html", projects=projects)
 
-@app.route('/project/<project_id>')
-def project(project_id): 
-    project = db.get_project(project_id)
 
-    print(project)
+@app.route("/project/<project_id>")
+def project(project_id):
+    project = db.get_project(project_id)
 
     if len(project) == 1:
         project = project[0]
--- db.py
+++ db.py
@@ -3,15 +3,21 @@ from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
 
 # Point to Hasura and pass in the admin secret
-transport = AIOHTTPTransport(url="https://gql.rcos.io/v1/graphql", headers={
-    "content-type": "application/json", "x-hasura-admin-secret": os.environ.get('HASURA_SECRET') 
-})
+transport = AIOHTTPTransport(
+    url="https://gql.rcos.io/v1/graphql",
+    headers={
+        "content-type": "application/json",
+        "x-hasura-admin-secret": os.environ.get("HASURA_SECRET"),
+    },
+)
 
 # Create a GraphQL client
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
+
 def get_project(project_id: str):
-    query = gql("""
+    query = gql(
+        """
         query GetProject($id: Int!) {
           projects(where: {project_id: {_eq: $id}}) {
             title
@@ -22,13 +28,16 @@ def get_project(project_id: str):
           }
         }
 
-    """)
+    """
+    )
 
-    result = client.execute(query, variable_values={ "id": project_id })
+    result = client.execute(query, variable_values={"id": project_id})
     return result["projects"]
 
+
 def get_semester_projects(semester: str, with_enrollments: bool):
-    query = gql("""
+    query = gql(
+        """
         query SemesterProjects($semesterIdOrTitle: String!, $withEnrollments: Boolean!) {
             projects(order_by: {title: asc}, where: {enrollments: {_or: [{semester_id: {_eq: $semesterIdOrTitle}}, {semester: {title: {_ilike: $semesterIdOrTitle}}}]}}) {
                 project_id
@@ -46,8 +55,15 @@ def get_semester_projects(semester: str, with_enrollments: bool):
                 }
             }
         }
-    """)
+    """
+    )
 
-    result = client.execute(query, variable_values={"semesterIdOrTitle": semester, "withEnrollments": with_enrollments})
+    result = client.execute(
+        query,
+        variable_values={
+            "semesterIdOrTitle": semester,
+            "withEnrollments": with_enrollments,
+        },
+    )
+
     return result["projects"]
-
